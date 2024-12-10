3c5016eb25a2663d5b7d05e2a6c76e2a4115546c
tokaahmed345 <tokaahmed34567@gmail.com>
2024-12-10 03:26:05 +0200

    display listOfItem and  make responsive UI

--- lib/screens/azkar.dart
+++ lib/screens/azkar.dart
@@ -1,56 +1,84 @@
+import 'dart:convert';
 import 'package:alquran/screens/azkar_contetnt.dart';
-import 'package:alquran/widgets/item_row.dart';
 import 'package:flutter/material.dart';
 
 class Al_Azkar extends StatelessWidget {
   const Al_Azkar({super.key});
-static String id="azkar";
+  static String id = "azkar";
+
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-
-appBar: AppBar(title: Text("Al_Azkar"),
-backgroundColor: Color(0xfff0ede8),
-centerTitle: true,
-leading: Icon(Icons.arrow_back_ios,size: 35,),
-),
-
-body: Stack(
-
-  children:[
-    Positioned.fill(child: Image.asset("assets/images/back.jpg")),
-    
-     ListView.builder(
-    itemCount: 5,
-    itemBuilder: (context,index){
-    return  Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: InkWell(
-        onTap: (){
-          Navigator.pushNamed(context, Azkar_Content.id);
-        },
-        child: ListTile(
-          
-             leading:    CircleAvatar(
-              child: Text(
-                "1",
-                style: TextStyle(
-                  fontSize: 23,
-                  color: Colors.white,
-                ),
-              ),
-              backgroundColor: Colors.black,
-            ),
-        
-           trailing:    Text("اذكار الصباح ",style: TextStyle(fontSize: 29),)
-        
-        
-        ,),
+      appBar: AppBar(
+        title: Text("Al_Azkar"),
+        backgroundColor: Color(0xfff0ede8),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, size: 35),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-    );
-  }),
-]),
+      body: Stack(
+        children: [
+          Positioned.fill(child: Image.asset("assets/images/back.jpg")),
+          FutureBuilder(
+            future: DefaultAssetBundle.of(context).loadString("assets/images/azkar.json"),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text("Error: ${snapshot.error.toString()}"),
+                );
+              }
 
+              if (snapshot.hasData) {
+                List azkarData = json.decode(snapshot.data!);
+
+                return LayoutBuilder(
+                  builder: (context, constraints) {
+                    return ListView.builder(
+                      itemCount: azkarData.length,
+                      itemBuilder: (context, index) {
+                        var azkar = azkarData[index];
+
+                        double fontSize = screenSize.width > 600 ? 29 : 22;
+
+                        return Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, Azkar_Content.id);
+                            },
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                child: Text(
+                                  azkar['id'].toString(),
+                                  style: TextStyle(fontSize: 23, color: Colors.white,),
+                                ),
+                                backgroundColor: Colors.black,
+                                
+                              ),
+                              trailing: Text(
+                                azkar['name'],
+                                style: TextStyle(fontSize: fontSize,fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/azkar_contetnt.dart
+++ lib/screens/azkar_contetnt.dart
@@ -5,6 +5,27 @@ class Azkar_Content extends StatelessWidget {
 static String id="Azkar_Content";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(appBar: AppBar(
+      title: Text("اذكار الصاح "),
+        backgroundColor: Color(0xfff0ede8),
+        centerTitle: true,
+        leading: IconButton(
+         icon: Icon(Icons.arrow_back_ios,size: 35,),
+
+onPressed: 
+(){
+  Navigator.pop(context);
+},
+        ),
+      ),
+      
+      body: Stack(
+        children: [
+          Positioned.fill(child: Image.asset("assets/images/back.jpg")),
+        ]
+      
+      )
+    );
+      
   }
 }
